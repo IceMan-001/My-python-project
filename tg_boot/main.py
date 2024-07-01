531c08c0d9948ace5b822f002b5d29db8ef0a32d
Andrey Bobrov <8867006@mail.ru>
2024-07-01 17:27:18 +0300

    Продолжается разработка бота.

--- tg_boot/main.py
+++ tg_boot/main.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from aiogram.types import Message
 
@@ -17,15 +17,44 @@ dp = Dispatcher()
 # этот хэндлер срабатывает на команду /start
 @dp.message(Command(commands=['start']))
 async def process_command_start(massage: Message):
-    await massage.answer('Привет! меня зовут: Топ-бот \n Напиши мне что-нибудь!')
+    await massage.answer(
+        'Привет! Меня зовут "Bot-Advisor"! \n Для обзора возможностей бота воспользуйтесь командой \n"help"')
+
+
+@dp.message(Command(commands=['help']))
+async def process_command_help(massage: Message):
+    await massage.answer('Для вывода прогноза погоды: \n введите команду: "/weather" или воспользуйтесь пунктом меню.')
+    await massage.answer(
+        'Для вывода прогноза курса валют: \n введите команду: "/courses" или воспользуйтесь пунктом меню.')
+    await massage.answer(
+        'Для вывода прогноза вакансий: \n введите команду: "/vacancies" или  воспользуйтесь пунктом меню.')
+
+
+@dp.message(Command(commands=['weather']))
+async def process_command_weather(massage: Message):
+    await massage.answer('Погода')
+
+
+@dp.message(Command(commands=['courses']))
+async def process_command_courses(massage: Message):
+    await massage.answer('Курс валют')
+
+
+@dp.message(Command(commands=['vacancies']))
+async def process_command_vacancies(massage: Message):
+    await massage.answer('Подбор вакансий')
+
+
+@dp.message(lambda msg: 'vacancies' in msg.text.lower())
+async def answer(message: types.Message):
+    await message.answer('По вашему запросу будет подготовлен и выведен ответ через 1, 2, 3...')
 
 
 @dp.message()
-async def process_command_start(massage: Message):
-    await massage.answer('Привет! меня зовут: Топ-бот \n Напиши мне что-нибудь!')
-    await massage.answer('Привет! меня зовут: Топ-бот \n Напиши мне что-нибудь!')
-    await massage.answer('Привет! меня зовут: Топ-бот \n Напиши мне что-нибудь!')
+async def answer(message: types.Message):
+    await message.answer('Команда не опознана! \nДля просмотра возможных команд \nвоспользуйтесь командой: "help"')
+
 
 if __name__ == "__main__":
     dp.run_polling(bot)
-
+    print("\N{smiling face with sunglasses}""\N{smiling face with sunglasses}""\N{smiling face with sunglasses}")
